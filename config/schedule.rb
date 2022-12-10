72ba2862e486b0760b3119ee9188bf3e9505cff6
Fred Willis <fdwillis7@icloud.com>
2022-12-10 00:42:20 -0500

    syntax?

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 require 'clockwork'
 include Clockwork
 
-every(1.day, 'issueProfit.ifCleared', :at => '00:00') {rake 'issueProfit:ifCleared'}
+every(1.day, 'issueProfit.ifCleared', :at => '00:00') {'rake issueProfit:ifCleared'}
